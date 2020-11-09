104c6a034d2561b40fbce3972308c739835c5954
Chinami Kitagawa <cnm25.k@gmail.com>
2020-11-09 06:25:05 +0000

    create model Book

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_07_020816) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
