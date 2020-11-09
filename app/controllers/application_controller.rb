cd4bb478029082ec314b032e6fd747506f4a15bd
Chinami Kitagawa <cnm25.k@gmail.com>
2020-11-09 06:57:01 +0000

    create user's show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(current_user.id)
   end
 
   def after_sign_out_path_for(reaource)
