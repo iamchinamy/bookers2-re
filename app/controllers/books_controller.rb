e452e17bb8a2b2685dc7b03967086ae54859c21c
Chinami Kitagawa <cnm25.k@gmail.com>
2020-11-14 00:01:14 +0000

    create book edit.html.slim

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,6 +28,13 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have updated book successfully"
+    else
+      render action: :edit
+    end
   end
 
   def destroy
